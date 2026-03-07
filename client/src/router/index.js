8e33eadb7e7550cc04bb93bb254bf1007b87facd
neohral <honekingu29@gmail.com>
2026-03-07 17:04:41 +0900

    fix default

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,7 +6,7 @@ import Intro from '../views/Intro.vue'
 const routes = [
   { path: '/party/:roomId', component: Party },
   { path: '/intro/:roomId', component: Intro },
-  { path: '/', redirect: to => ({ path: `/intro/default` }) }
+  { path: '/', redirect: to => ({ path: `/party/default` }) }
 ]
 
 export default createRouter({
